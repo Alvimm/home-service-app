171e4b1d4da2b89b61ef66541e6877b5e89031da
alvimm <filipe2012alvim@gmail.com>
2024-01-16 00:28:51 -0300

    Add Heading and PageHeading components

---
+++ App/Components/PageHeading.jsx
@@ -0,0 +1,24 @@
+import { View, Text, TouchableOpacity} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation} from '@react-navigation/native';
+
+export default function PageHeading({title}) {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: 10,
+        alignItems: 'center',
+      }}
+    >
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Ionicons name="arrow-back-outline" size={30} color="black" />
+      </TouchableOpacity>
+      <Text style={{ fontSize: 25, fontFamily: 'outfit-medium' }}>
+        {title}
+      </Text>
+    </View>
+  );
+}
